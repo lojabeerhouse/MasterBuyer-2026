b685eaa27da5de80bce4578de62d440f74e65226
beerhousearapongas <beerhousearapongas@gmail.com>
2026-03-19 19:03:10 +0000

    vite.config.ts att

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
     const isProd = mode === 'production';
 
     return {
-      base: isProd ? '/MasterBuyer-2026/' : '/',
+      base: isProd ? '/MasterBuyer-2026/app' : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
